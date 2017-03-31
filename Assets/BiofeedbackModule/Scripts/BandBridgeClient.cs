99ec5e9f131e8141dae128551fe90c912a2521f6
Soskat <soskat@outlook.com>
2017-03-31 08:40:51 +0200

    Communication with BandBridge server now works in background

--- Assets/BiofeedbackModule/Scripts/BandBridgeClient.cs
+++ Assets/BiofeedbackModule/Scripts/BandBridgeClient.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using Communication.Client;
 using Communication.Data;
+using System.ComponentModel;
 
 public class BandBridgeClient : MonoBehaviour {
 
@@ -25,10 +26,30 @@ public class BandBridgeClient : MonoBehaviour {
         if(GUI.Button(new Rect(50, 50, 100, 30), "Connect test"))
         {
             Message message = new Message(Command.SHOW_ASK, null);
-            Debug.Log(">>__La message: " + message);
-
-            Message response = SocketClient.StartClient(HostName, ServicePort, message);
-            Debug.Log(">>__La response: " + response);
+            SendMessageToBandBridge(message);
         }
+    }
+
+
+    private void SendMessageToBandBridge(Message message)
+    {
+        // original source: http://stackoverflow.com/a/34040733
+
+        Debug.Log("Prepaired message: " + message);
+
+        BackgroundWorker worker = new BackgroundWorker();
+        worker.DoWork += (s, e) => {
+            e.Result = SocketClient.StartClient(HostName, ServicePort, message);
+        };
+        worker.RunWorkerCompleted += (s, e) => {
+            DealWithResponse((Message)e.Result);
+        };
+        worker.RunWorkerAsync();
+    }
+
+
+    private void DealWithResponse(Message response)
+    {
+        Debug.Log("Received response: " + response);
     }
 }
