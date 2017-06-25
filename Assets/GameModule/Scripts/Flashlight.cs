bef41e854832bae64f0cea23de07b31c6a8c25f1
Soskat <soskat@outlook.com>
2017-06-25 17:00:30 +0200

    Added basic Player prefab with flashlight

---
+++ Assets/GameModule/Scripts/Flashlight.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+
+namespace LastBastion.Game
+{
+    public class Flashlight : MonoBehaviour
+    {
+        #region Private fields
+        [SerializeField] private bool lightOn = false;
+        [SerializeField] private GameObject light;
+        #endregion
+
+
+        #region Public fields & properties
+        /// <summary>Is light turned on?</summary>
+        public bool LightOn { get { return lightOn; } }
+        #endregion
+
+
+        #region MonoBehaviour methods
+        // Awake is called when the script instance is being loaded
+        private void Awake()
+        {
+            Assert.IsNotNull(light);
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            light.SetActive(false);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+        #endregion
+
+
+        #region Public methods
+        /// <summary>
+        /// Switches the light of the flashlight.
+        /// </summary>
+        public void SwitchLight()
+        {
+            lightOn = (lightOn) ? false : true;
+            if (lightOn) light.SetActive(true);
+            else light.SetActive(false);
+        }
+        #endregion
+    }
+}
---
+++ Assets/GameModule/Scripts/Player/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace LastBastion.Game.Player
+{
+    public class Player : MonoBehaviour
+    {
+
+        #region Private fields
+        private Flashlight flashlight;
+        #endregion
+
+
+        // Use this for initialization
+        void Start()
+        {
+            flashlight = GetComponentInChildren<Flashlight>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                flashlight.SwitchLight();
+            }
+        }
+    }
+}
