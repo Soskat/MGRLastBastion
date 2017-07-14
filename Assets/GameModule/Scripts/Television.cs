54edc322e207ce1ed308dcc198bfc00dff0d461a
Soskat <soskat@outlook.com>
2017-07-14 22:32:48 +0200

    Added television animation

---
+++ Assets/GameModule/Scripts/Television.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Television : MonoBehaviour {
+
+    #region Private fields
+    [SerializeField] private bool isOn = false; // ------------------- for testing
+    private Light monitorLight;
+    private Material material;
+    private Color glassColor;
+    private float hue;
+    private float saturation;
+    private float value;
+
+    #endregion
+
+
+    #region MonoBehaviour methods
+    // Use this for initialization
+    void Start()
+    {
+        monitorLight = GetComponentInChildren<Light>();
+        material = GetComponent<Renderer>().material;
+        glassColor = material.GetColor("_EmissionColor");
+        TurnOffTV();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // for testing purposes:
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            if (isOn)
+            {
+                TurnOnTV();
+                isOn = false;
+            }
+            else
+            {
+                TurnOffTV();
+                isOn = true;
+            }
+        }
+    }
+    #endregion
+    
+
+    #region Private methods
+    /// <summary>
+    /// Turns off the television.
+    /// </summary>
+    private void TurnOffTV()
+    {
+        StopAllCoroutines();
+        monitorLight.gameObject.SetActive(false);
+        material.SetColor("_EmissionColor", Color.black);
+    }
+
+    /// <summary>
+    /// Turns on the television.
+    /// </summary>
+    private void TurnOnTV()
+    {
+        // enable light:
+        monitorLight.gameObject.SetActive(true);
+        monitorLight.intensity = 1.5f;
+        // change monitor material color:
+        material.SetColor("_EmissionColor", glassColor);
+        material.SetFloat("_EmissionColorUI", monitorLight.intensity - 1f);
+        // start blinking:
+        StartCoroutine(Blink());
+    }
+
+    /// <summary>
+    /// Coroutine that simulates blinking of the television monitor.
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator Blink()
+    {
+        // get current hue, saturation and value of material color:
+        Color.RGBToHSV(material.GetColor("_EmissionColor"), out hue, out saturation, out value);
+        // set new light intensity and color value:
+        monitorLight.intensity = Random.Range(1.0f, 2.0f);
+        value = monitorLight.intensity - 1f;
+        // set new material color:
+        material.SetColor("_EmissionColor", Color.HSVToRGB(hue, saturation, value));
+        yield return new WaitForSeconds(Random.Range(0.05f, 0.1f));
+        StartCoroutine(Blink());
+    }
+    #endregion
+}
