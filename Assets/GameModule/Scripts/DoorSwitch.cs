872e66b2f2e37e0d84c97e843d189e55b870d46f
Soskat <soskat@outlook.com>
2017-07-15 17:43:08 +0200

    Basic logic for isolation ward door switches

---
+++ Assets/GameModule/Scripts/DoorSwitch.cs
@@ -0,0 +1,32 @@
+﻿using LastBastion.Game;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Component that represents door switcher behaviour.
+/// </summary>
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(Animator))]
+public class DoorSwitch : MonoBehaviour {
+
+    #region Private fields
+    [SerializeField] private Door door;
+    private Animator animator;
+    #endregion
+
+
+    #region MonoBehaviour methods
+    // Use this for initialization
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    #endregion
+}
