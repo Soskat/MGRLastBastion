b404056465eff9cbb0b5d87fdcb85a705b86d28e
Soskat <soskat@outlook.com>
2017-07-30 16:20:54 +0200

    Updated lights settings

---
+++ Assets/GameModule/Scripts/_unused/SoundAreaActivator.cs
@@ -0,0 +1,30 @@
+﻿using LastBastion.Game.Managers;
+using UnityEngine;
+
+
+namespace LastBastion.Game
+{
+    /// <summary>
+    /// Component that manages the IsActive status of assigned area.
+    /// </summary>
+    [RequireComponent(typeof(Rigidbody))]
+    public class SoundAreaActivator : MonoBehaviour
+    {
+        #region Private fields
+        [SerializeField] private SoundManager watchedArea;
+        #endregion
+
+
+        #region MonoBehaviour methods
+        // OnCollisionEnter is called when this collider/rigidbody has begun touching another rigidbody/collider
+        private void OnCollisionExit(Collision collision)
+        {
+            if (GameManager.instance.ActiveSoundArea != null)
+                GameManager.instance.ActiveSoundArea.IsActive = false;
+
+            watchedArea.IsActive = true;
+            GameManager.instance.ActiveSoundArea = watchedArea;
+        }
+        #endregion
+    }
+}
