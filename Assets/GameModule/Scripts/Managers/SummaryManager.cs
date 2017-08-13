c2d2d4e99a3ab0280c704ad7d91ea9869b3c56ad
Soskat <soskat@outlook.com>
2017-08-13 18:21:06 +0200

    Fixing bugs in game flow, part II

--- Assets/GameModule/Scripts/Managers/SummaryManager.cs
+++ Assets/GameModule/Scripts/Managers/SummaryManager.cs
@@ -1,5 +1,6 @@
 ﻿using LastBastion.Game.UIControllers;
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.UI;
@@ -15,10 +16,13 @@ namespace LastBastion.Game.Managers
         #region Private fields
         [SerializeField] private Button endSceneButton;
         [SerializeField] private Button backToMainMenuButton;
+        [SerializeField] private Text timerText;
+        [SerializeField] private Text nextLevelText;
         [SerializeField] private AchievementPanelController timeAchievement;
         [SerializeField] private AchievementPanelController runesAchievement;
         [SerializeField] private AchievementPanelController doorsAchievement;
         [SerializeField] private AchievementPanelController lightSwitchAchievement;
+        [SerializeField] private int secondsToGo = 120;
         #endregion
 
 
@@ -26,6 +30,8 @@ namespace LastBastion.Game.Managers
         // Awake is called when the script instance is being loaded
         private void Awake()
         {
+            Assert.IsNotNull(timerText);
+            Assert.IsNotNull(nextLevelText);
             Assert.IsNotNull(timeAchievement);
             Assert.IsNotNull(runesAchievement);
             Assert.IsNotNull(doorsAchievement);
@@ -36,8 +42,18 @@ namespace LastBastion.Game.Managers
         void Start()
         {
             // update buttons behaviour:
-            endSceneButton.onClick.AddListener(() => { GameManager.instance.LevelHasEnded(); });
-            backToMainMenuButton.onClick.AddListener(() => { GameManager.instance.BackToMainMenu(); });
+            if (GameManager.instance.DebugMode)
+            {
+                endSceneButton.onClick.AddListener(() => { GameManager.instance.LevelHasEnded(); });
+                endSceneButton.gameObject.SetActive(true);
+                backToMainMenuButton.onClick.AddListener(() => { GameManager.instance.BackToMainMenu(); });
+                backToMainMenuButton.gameObject.SetActive(true);
+            }
+            else
+            {
+                endSceneButton.gameObject.SetActive(false);
+                backToMainMenuButton.gameObject.SetActive(false);
+            }
 
             // update achievements data:
             UpdateAchievementsPanels();
@@ -45,12 +61,9 @@ namespace LastBastion.Game.Managers
             // unlock cursor state after game level:
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
-        }
 
-        // Update is called once per frame
-        void Update()
-        {
-
+            // run stopwatch:
+            StartCoroutine(Stopwatch());
         }
         #endregion
 
@@ -67,6 +80,27 @@ namespace LastBastion.Game.Managers
             runesAchievement.UpdateAchievementData("runes achik >>", GameManager.instance.CollectedRunes.ToString());
             doorsAchievement.UpdateAchievementData("doors achik >>", GameManager.instance.OpenedDoors.ToString());
             lightSwitchAchievement.UpdateAchievementData("light switch achik >>", GameManager.instance.LightSwitchUses.ToString());
+        }
+
+        /// <summary>
+        /// Simple coroutine that simulates timer.
+        /// </summary>
+        /// <returns></returns>
+        private IEnumerator Stopwatch()
+        {
+            int minutes;
+            int seconds;
+            for (int i = secondsToGo; i >= 0; i--)
+            {
+                minutes = i / 60;
+                seconds = i - (minutes * 60);
+                timerText.text = String.Format("{0:00}:{1:00}", minutes, seconds);
+                yield return new WaitForSeconds(1.0f);
+            }
+            nextLevelText.text = "( Loading next scene )";
+            timerText.gameObject.SetActive(false);
+            // inform that level has ended:
+            GameManager.instance.LevelHasEnded();
         }
         #endregion
     }
